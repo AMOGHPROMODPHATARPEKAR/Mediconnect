568d2ef7ac3477382e69b087c7b1e5ded6fbd58b
AMOGHPROMODPHATARPEKAR <amoghpp129@gmail.com>
2024-05-20 19:29:05 +0530

    first

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./app.css";
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className=' bg-blue-500 text-cyan-400 font-serif   '>Hello sdsd</h1>
+      
+    </>
+  )
+}
+
+export default App
